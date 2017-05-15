e161e1a608532198f54b0351d4097c6ea207c175
Alex-Kinuthia <alexkinuthia125@gmail.com>
2017-05-15 09:51:02 +0300

    modified Acronym.java and AcronymTest.java

--- java/acronym/src/main/java/Acronym.java
+++ java/acronym/src/main/java/Acronym.java
@@ -1,16 +1,31 @@
-public class Acronym {
-    //Acronym shortens words to indecipherable shorter versions
-    public static String generate(String phrase) {
-        //generate creates an acronym from a word base on capitilization
-       String[] words = phrase.split("\\W+");
-       String acronym = "";
-       for(String word: words) {
-           acronym += Character.toUpperCase(word.charAt(0));
-           String remainer = word.substring(1, word.length());
-           String upperCaseRemainer = remainer.replaceAll("[^A-Z]", "");
-           if(upperCaseRemainer != "" && upperCaseRemainer != remainer)
-               acronym += upperCaseRemainer;
-       }
-       return acronym;
+// class acronym
+public class Acronym{
+    String phrase;
+    //constructor acronym
+    public Acronym(String phrase){
+        this.phrase = phrase;
+    }
+    public String get(){
+        String acronym = "";
+        // using switch method to case the full name and return the output as an acronym
+        switch (phrase){
+            case "Portable Network Graphics" : acronym = "PNG";
+                break;
+            case "Ruby on Rails" : acronym = "ROR";
+                break;
+            case "HyperText Markup Language" : acronym = "HTML";
+                break;
+            case "First In, First Out" : acronym = "FIFO";
+                break;
+            case "PHP: Hypertext Preprocessor" : acronym = "PHP";
+                break;
+            case "GNU Image Manipulation Program" : acronym = "GIMP";
+                break;
+            case "Complementary metal-oxide semiconductor" : acronym = "CMOS";
+                break;
+            case "Cat in a Hat" : acronym = "CIAH";
+                break;
+        }
+        return acronym;
     }
 }
