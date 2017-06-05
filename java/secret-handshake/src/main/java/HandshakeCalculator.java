e2ab53e48c14bd1fb16771c4571b2d74890fbaa3
Alex-Kinuthia <alexkinuthia125@gmail.com>
2017-06-05 11:10:23 +0300

    worked on a new solution -"SecretHandShake"

--- java/secret-handshake/src/main/java/HandshakeCalculator.java
+++ java/secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,24 +1,25 @@
 import java.util.List;
-import java.util.LinkedLIst;
-import java.util.collections;
+import java.util.LinkedList;
+import java.util.Collections;
 
+// class HandshakeCalculator
 final class HandshakeCalculator {
  public boolean shouldWink(int n) {
    return (n & 1) == 1;
  }
-
- public boolean shouldDoubleBlink(int n)
+// input of 2 shouldDoubleBlink
+ public boolean shouldDoubleBlink(int n) {
  return (n & 2) == 2;
  }
-
+// input of 4 shouldCloseYourEyes
  public boolean shouldCloseYourEyes(int n) {
    return (n & 4) == 4;
  }
-
+// input of 8 should result to jump
  public boolean shouldJump(int n) {
       return (n & 8) == 8;
   }
-
+// input of 16 shuld result to reverse
   public boolean shouldReverse(int n) {
       return (n & 16) == 16;
   }
